e5fc25f3f63ca0320e9d724befe6778567f30037
wbisoles1214 <wbisoles@gmail.com>
2023-04-26 11:08:12 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// Load the Google Custom Search Engine script asynchronously
+const script = document.createElement("script");
+script.async = true;
+script.src = "https://cse.google.com/cse.js?cx=12cc501042a7a47f0";
+document.head.appendChild(script);
+
+// Wait for the script to load
+script.addEventListener("load", () => {
+  // Get the search element from the HTML
+  const searchElement = document.querySelector(".gcse-search");
+
+  // Set the data-api-key attribute
+  searchElement.setAttribute(
+    "data-api-key",
+    "AIzaSyDCCPQ4F535kWIbVLwDHFUWe7MAgyFsbKo"
+  );
+});
