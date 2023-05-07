73c5bc4e0677d055b53020b0bded23e803837f8c
wbisoles1214 <wbisoles@gmail.com>
2023-05-07 17:12:17 +0800

    I transfer the files outside of the e26 module to become easy to browse on the github pages and added two folders in src named gmap folder and temp-app

--- MP2-PROJECT/src/darkmode.js
+++
@@ -1,8 +0,0 @@
-export function setupDarkMode() {
-  let content = document.getElementsByTagName("body")[0];
-  let darkMode = document.getElementById("dark-change");
-  darkMode.addEventListener("click", function () {
-    darkMode.classList.toggle("active");
-    content.classList.toggle("night");
-  });
-}
--- MP2-PROJECT/src/utilities-function/getRandomInt.js
+++
@@ -1,6 +0,0 @@
-export function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-let randomNumber = getRandomInt(0, 1000);
-
-export default getRandomInt;
